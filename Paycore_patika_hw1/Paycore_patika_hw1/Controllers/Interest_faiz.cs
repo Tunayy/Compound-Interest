3d56d4209f236b0df50d437d066b944c950340f4
Tunay Türer <56853506+Tunayy@users.noreply.github.com>
2022-09-09 00:12:57 +0300

    Add files via upload

---
+++ Paycore_patika_hw1/Paycore_patika_hw1/Controllers/Interest_faiz.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Paycore_patika_hw1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Interest_faiz : ControllerBase
+    {
+
+        [HttpGet("Compound İnterest Calculation")]
+        public string Get(float MainMoney, double compound , double paried_amount)
+        {
+            double anlik_Sureli = 2.71;
+
+            for (int i = 0; i < paried_amount * compound - 1; i++)
+            {
+                anlik_Sureli *= 2.71;
+            }
+            double FaizGetirisi = MainMoney * anlik_Sureli;
+            return "Toplam Bakiye: " + Convert.ToString(FaizGetirisi) + " Toplam Kazanılan Faiz Tutarı: " + Convert.ToString(FaizGetirisi - MainMoney);
+        }
+    }
+}
